0bcaf3521651d79c47a47b632d3fa0a11d98c41e
Andrew Dimmer <contactandrewd@gmail.com>
2019-01-19 16:12:48 -0500

    Updated the template, sidebar, and navigation
    Made a wide range of changes to get the template ready for content.

---
+++ scripts/navbarscripts.js
@@ -0,0 +1,7 @@
+function toggleNav() {
+    if (document.getElementById("sidenav").style.display.indexOf("block") > -1) {
+        document.getElementById("sidenav").style.display = "none";
+    } else {
+        document.getElementById("sidenav").style.display = "block";
+    }
+} 
